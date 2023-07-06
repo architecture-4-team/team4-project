f3504643af8f729a31460319ad0fced73f2bc14c
warebowl <warebowl@gmail.com>
2023-07-06 18:20:20 +0900

    Update client.py
    server application 과 프로토콜을 연동하기 위한 test client 앱

--- server_test_app/client.py
+++ server_test_app/client.py
@@ -8,7 +8,11 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 HOST = '192.168.56.103'  # 서버의 IP 주소
 PORT = 10000  # 서버의 포트 번호
-
+ID = 'warebowl@hotmail.com'
+TARGET_ID = 'abc@gmail.com'
+PWD = '12345678'
+UUID = ''
+callid = ''
 
 # JSON 데이터를 서버로 전송하는 함수
 def send_json_data(sock, data):
@@ -19,18 +23,27 @@ def send_json_data(sock, data):
 # JSON 데이터를 수신하는 함수
 def receive_json_data(sock):
     json_data = sock.recv(1024).decode()
+    print(json_data, type(json_data))
     data = json.loads(json_data)  # JSON 역직렬화
     return data
 
 
 # 수신 쓰레드에서 실행될 함수
 def receive_thread(sock):
+    global UUID, callid
     while True:
         try:
             received_data = receive_json_data(sock)
-            print('Received data from server:', received_data)
+            print('Received data from server:', received_data, type(received_data))
+            UUID = received_data['contents']['uuid']
+            print(UUID)
+
+            if "callid" in received_data['contents']:
+                callid = received_data['contents']['callid']
+                print(callid)
+
         except json.JSONDecodeError:
-            print('Invalid JSON format')
+            print('recv Invalid JSON format')
             break
         except ConnectionError:
             print('Disconnected from server')
@@ -77,7 +90,7 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle('Button Example')
-        self.resize(300, 600)
+        self.resize(300, 400)
 
         # 버튼 생성 및 설정
         self.button1 = QPushButton('Login', self)
@@ -88,24 +101,54 @@ class MainWindow(QMainWindow):
         self.button2.clicked.connect(self.button2_clicked)
         self.button2.setGeometry(50, 100, 100, 30)
 
+
+        self.button3 = QPushButton('INVITE', self)
+        self.button3.clicked.connect(self.button3_clicked)
+        self.button3.setGeometry(50, 150, 100, 30)
+
+
+        self.button4 = QPushButton('ACCEPT', self)
+        self.button4.clicked.connect(self.button4_clicked)
+        self.button4.setGeometry(50, 200, 100, 30)
+
+
+        self.button5 = QPushButton('CANCEL', self)
+        self.button5.clicked.connect(self.button5_clicked)
+        self.button5.setGeometry(50, 250, 100, 30)
+
+
+        self.button6 = QPushButton('BYE', self)
+        self.button6.clicked.connect(self.button6_clicked)
+        self.button6.setGeometry(50, 300, 100, 30)
+
+
         # 추가 버튼 생성
-        self.additional_buttons = []
-        for i in range(3, 11):
-            button = QPushButton(f'Button {i}', self)
-            button.clicked.connect(lambda _, num=i: self.additional_button_clicked(num))
-            button.setGeometry(50, 150 + (i-3) * 50, 100, 30)
-            self.additional_buttons.append(button)
+        # self.additional_buttons = []
+        # for i in range(3, 11):
+        #     button = QPushButton(f'Button {i}', self)
+        #     button.clicked.connect(lambda _, num=i: self.additional_button_clicked(num))
+        #     button.setGeometry(50, 150 + (i-3) * 50, 100, 30)
+        #     self.additional_buttons.append(button)
 
     def button1_clicked(self):
-        print('Button 1 clicked')
+        global ID
+        global PWD
+        print('**** LOGIN ****')
         # 버튼 1을 클릭했을 때 실행할 코드를 여기에 추가
+        # data = '''{
+        #         "command": "LOGIN",
+        #         "contents": {
+        #             "email": "warebowl@gmail.com",
+        #             "password": "12345678"
+        #           }
+        #         }'''
         data = '''{
-        "command": "LOGIN", 
-        "contents": {
-            "email": "warebowl@gmail.com",
-            "password": "12345678"
-          }
-        }'''
+            "command": "LOGIN", 
+            "contents": {
+                "email": "%s",
+                "password": "%s"
+              }
+            }''' % (ID, PWD)
         try:
             json_data = json.loads(data)  # JSON 파싱
             send_json_data(sock, json_data)
@@ -113,20 +156,106 @@ class MainWindow(QMainWindow):
             print('Invalid JSON format')
 
     def button2_clicked(self):
-        print('Button 2 clicked')
+        global UUID
+        global ID
+        print('**** LOGOUT ****')
         # 버튼 2를 클릭했을 때 실행할 코드를 여기에 추가
         data = '''{
-                "command": "LOGOUT" 
-                }'''
+            "command": "LOGOUT",
+            "contents": {
+                "uuid": "%s",
+                "email": "%s"
+              }
+            }''' % (UUID, ID)
         try:
             json_data = json.loads(data)  # JSON 파싱
             send_json_data(sock, json_data)
         except json.JSONDecodeError:
             print('Invalid JSON format')
 
-    def additional_button_clicked(self, num):
-        print(f'Additional Button {num} clicked')
-        # 추가 버튼을 클릭했을 때 실행할 코드를 여기에 추가
+    def button3_clicked(self):
+        global UUID
+        global ID
+        print('**** INVITE ****')
+        # 버튼 3을 클릭했을 때 실행할 코드를 여기에 추가
+        data = '''{
+                    "command": "INVITE",
+                    "contents": {
+                        "uuid": "%s",
+                        "target": "%s"
+                      }
+                    }''' % (UUID, TARGET_ID)
+        print(type(data))
+        try:
+            json_data = json.loads(data)  # JSON 파싱
+            print(type(json_data))
+            send_json_data(sock, json_data)
+        except json.JSONDecodeError:
+            print('button3 Invalid JSON format')
+
+    def button4_clicked(self):
+        global callid, UUID
+        print('**** ACCEPT ****')
+        # 버튼 4을 클릭했을 때 실행할 코드를 여기에 추가
+        data = '''{
+            "command": "ACCEPT",
+            "contents": {
+                "uuid": "%s",
+                "callid": "%s"
+              }
+            }''' % (UUID, callid)
+        print(type(data))
+        try:
+            json_data = json.loads(data)  # JSON 파싱
+            print(type(json_data))
+            send_json_data(sock, json_data)
+        except json.JSONDecodeError:
+            print('button3 Invalid JSON format')
+        pass
+
+    def button5_clicked(self):
+        global callid, UUID
+        print('**** CANCEL ****')
+        # 버튼 4을 클릭했을 때 실행할 코드를 여기에 추가
+        data = '''{
+                    "command": "CANCEL",
+                    "contents": {
+                        "uuid": "%s",
+                        "callid": "%s"
+                      }
+                    }''' % (UUID, callid)
+        print(type(data))
+        try:
+            json_data = json.loads(data)  # JSON 파싱
+            print(type(json_data))
+            send_json_data(sock, json_data)
+        except json.JSONDecodeError:
+            print('button3 Invalid JSON format')
+        pass
+
+    def button6_clicked(self):
+        global callid, UUID
+        print('**** BYE ****')
+        # 버튼 4을 클릭했을 때 실행할 코드를 여기에 추가
+        data = '''{
+            "command": "BYE",
+            "contents": {
+                "uuid": "%s",
+                "callid": "%s"
+              }
+            }''' % (UUID, callid)
+        print(type(data))
+        try:
+            json_data = json.loads(data)  # JSON 파싱
+            print(type(json_data))
+            send_json_data(sock, json_data)
+        except json.JSONDecodeError:
+            print('button3 Invalid JSON format')
+        pass
+
+    # def additional_button_clicked(self, num):
+    #     print(f'Additional Button {num} clicked')
+    #     # 추가 버튼을 클릭했을 때 실행할 코드를 여기에 추가
 
 
 if __name__ == '__main__':
@@ -134,4 +263,3 @@ if __name__ == '__main__':
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
-
