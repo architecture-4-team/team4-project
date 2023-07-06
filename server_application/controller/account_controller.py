5bf5ba7142d535aa35fdf52935003a9eace94319
Jinhyung Lee <jinhyunglee@Jinhyungs-MacBook-Pro.local>
2023-07-06 12:25:03 +0900

    init web server
    init web server

--- server_application/controller/account_controller.py
+++
@@ -1,8 +0,0 @@
-class AccountController:
-    def __init__(self, main_window, main_view, account_view):
-        self.main_window = main_window
-        self.main_view = main_view
-        self.account_view = account_view
-
-    def account_button_clicked(self):
-        self.main_window.stacked_widget.setCurrentWidget(self.main_view)
