44e3bb09ae096709c305afda36137fda8a9fbd68
Jinhyung Lee <jinhyunglee@Jinhyungs-MacBook-Pro.local>
2023-07-12 20:46:42 +0900

    Add database backup
    1. Add database backup
    2. Update database schema

--- mysql/initdb.d/create_table.sql
+++ mysql/initdb.d/create_table.sql
@@ -1,23 +1,23 @@
 CREATE TABLE `user_table` (
-  `uuid` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `contact_id` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `pwd` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `firstname` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lastname` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ip` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pwd` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ip` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `online` tinyint DEFAULT '1',
   `enable` tinyint DEFAULT '1',
-  `summary` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `question1` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `question2` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `question3` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `summary` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `question1` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `question2` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `question3` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`uuid`),
   UNIQUE KEY `idx_user_table_email_pwd` (`email`,`pwd`),
   KEY `idx_users_contact_id` (`contact_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
 CREATE TABLE `contact_table` (
   `cid` int NOT NULL AUTO_INCREMENT,
@@ -31,17 +31,19 @@ CREATE TABLE `contact_table` (
   KEY `contact_table_ibfk_2` (`favorite_uid`),
   CONSTRAINT `contact_table_ibfk_1` FOREIGN KEY (`owner_uid`) REFERENCES `user_table` (`uuid`) ON DELETE CASCADE,
   CONSTRAINT `contact_table_ibfk_2` FOREIGN KEY (`favorite_uid`) REFERENCES `user_table` (`uuid`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
-CREATE TABLE IF NOT EXISTS `room_table` (
-    `room_id` varchar(64)  NOT NULL,
-    `uuid` varchar(64)  NOT NULL,
-    `aport` integer  DEFAULT 10001,
-    `vport` integer  DEFAULT 10002,
-    `created_at` timestamp  DEFAULT current_timestamp,
-    `updated_at` timestamp  DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`room_id`),
-    FOREIGN KEY (`uuid`) REFERENCES `user_table` (`uuid`) ON DELETE CASCADE
-);
+CREATE TABLE `room_table` (
+  `rid` int NOT NULL AUTO_INCREMENT,
+  `room_id` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `aport` int DEFAULT '10001',
+  `vport` int DEFAULT '10002',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`rid`),
+  KEY `uuid` (`uuid`),
+  CONSTRAINT `room_table_ibfk_1` FOREIGN KEY (`uuid`) REFERENCES `user_table` (`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
 CREATE INDEX `idx_users_contact_id` ON `user_table` (`contact_id`);
